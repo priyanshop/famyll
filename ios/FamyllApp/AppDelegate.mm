df19cc0e24bd4bd8adcda03f5cd1818a8b819995
Priyansh <priyansh4265@gmail.com>
2024-01-02 11:01:32 +0530

    Splash screen and icon in ios

--- ios/FamyllApp/AppDelegate.mm
+++ ios/FamyllApp/AppDelegate.mm
@@ -11,8 +11,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
   [RNSplashScreen show];
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
